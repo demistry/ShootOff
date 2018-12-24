4d18eae9036b5cc916bbb9bfd5a20b5b2ce4adf2
David Ilenwabor <dave11ilen@gmail.com>
2018-12-24 11:44:37 +0100

    Initial Commit

---
+++ ShootOffTests/ShootOffTests.swift
@@ -0,0 +1,34 @@
+//
+//  ShootOffTests.swift
+//  ShootOffTests
+//
+//  Created by David Ilenwabor on 24/12/2018.
+//  Copyright © 2018 David Ilenwabor. All rights reserved.
+//
+
+import XCTest
+@testable import ShootOff
+
+class ShootOffTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
