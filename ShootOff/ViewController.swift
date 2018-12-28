49265da6ae86b4a79a9d50a33a67ebd5d0cb1b58
David Ilenwabor <dave11ilen@gmail.com>
2018-12-28 12:52:16 +0100

    Gameplay introduced

--- ShootOff/ViewController.swift
+++ ShootOff/ViewController.swift
@@ -10,9 +10,88 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate , ARSessionDelegate{
 
     @IBOutlet var sceneView: ARSCNView!
+    
+    var trackingNode : SCNNode?
+    var smoothSurface = false
+    var tracking = true
+    
+    var directionalLight : SCNNode?
+    var ambientLight : SCNNode?
+    var container : SCNNode?
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        guard tracking else{
+            return
+        }
+        let hitTest = sceneView.hitTest(CGPoint(x: self.view.frame.midX, y: self.view.frame.midY), types: .featurePoint)
+        guard let result = hitTest.first else{
+            return
+        }
+        let translation = SCNMatrix4(result.worldTransform)
+        let position = SCNVector3Make(translation.m41, translation.m42, translation.m43)
+        
+        if trackingNode == nil{
+            let plane = SCNPlane(width: 0.15, height: 0.15)
+            plane.firstMaterial?.diffuse.contents = UIImage(named: "tracker.png")
+            plane.firstMaterial?.isDoubleSided = true
+            trackingNode = SCNNode(geometry: plane)
+            trackingNode?.eulerAngles.x = -.pi * 0.5
+            sceneView.scene.rootNode.addChildNode(self.trackingNode!)
+            smoothSurface = true
+        }
+        trackingNode?.position = position
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if tracking{
+            guard smoothSurface else{
+                return
+            }
+            let trackingPosition = trackingNode!.position
+            trackingNode?.removeFromParentNode()
+            container = sceneView.scene.rootNode.childNode(withName: "container", recursively: false)
+            container?.position = trackingPosition
+            container?.isHidden = false
+            
+            ambientLight = container?.childNode(withName: "ambient", recursively: false)
+            directionalLight = container?.childNode(withName: "directional", recursively: false)
+            tracking = false
+        } else{
+            guard let frame = sceneView.session.currentFrame else{
+                return
+            }
+            let camMatrix = SCNMatrix4(frame.camera.transform)
+            let direction = SCNVector3Make(-camMatrix.m31 * 5.0, -camMatrix.m32 * 10.0, -camMatrix.m33 * 5.0)
+            let position = SCNVector3Make(camMatrix.m41, camMatrix.m42, camMatrix.m43)
+            
+            let ball = SCNSphere(radius: 0.05)
+            ball.firstMaterial?.diffuse.contents = UIColor.blue
+            ball.firstMaterial?.emission.contents = UIColor.blue
+            let ballNode = SCNNode(geometry: ball)
+            ballNode.position = position
+            ballNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+            ballNode.physicsBody?.categoryBitMask = 3
+            ballNode.physicsBody?.contactTestBitMask = 1
+            sceneView.scene.rootNode.addChildNode(ballNode)
+            ballNode.runAction(SCNAction.sequence([SCNAction.wait(duration: 10.0), SCNAction.removeFromParentNode()]))
+            ballNode.physicsBody?.applyForce(direction, asImpulse: true)
+        }
+    }
+    
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        guard let light = frame.lightEstimate else{
+            return
+        }
+        guard !tracking else{
+            return
+        }
+        ambientLight?.light?.intensity = light.ambientIntensity * 0.4
+        directionalLight?.light?.intensity = light.ambientIntensity
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +103,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "art.scnassets/game_scene.scn")!
         
         // Set the scene to the view
         sceneView.scene = scene
@@ -35,6 +114,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.delegate = self
 
         // Run the view's session
         sceneView.session.run(configuration)
